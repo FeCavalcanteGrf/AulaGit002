541ffe14d19672df38fad599d8ce7eb49af0f3fe
FeCavalcanteGrf <felipercavalcante@gmail.com>
2025-02-24 21:18:10 -0300

    Primeiro commit

---
+++ Desafio.py
@@ -0,0 +1,25 @@
+# AleTavares/AulaPythonUniFaat
+# 1 Crie uma lista com 10 itens
+lista = ['banana', 'maça', 'uva', 'pera', 'laranja', 'melancia', 'melão', 'abacaxi', 'mamão', 'morango']
+
+# 2 Crie uma Tupla com 5 itens
+tupla = ('chatgpt', 'copilot', 'gemini', 'deepseek', 'claube')
+
+# 3 Crie um dicionario com 3 itens (codigo, nome, preço)
+dicionario = {
+    'codigo': 10,
+    'nome': "Testevaldo",
+    'preco': 30.0
+}
+
+# Crie uma função que imprima os itens da lista na tela
+def imprimir_lista():
+    print("\n")
+    print(lista)
+    print("\n")
+    print(tupla)
+    print("\n")
+    print(dicionario)
+    print("\n")
+
+imprimir_lista()
---
+++ operadoresAritimeticos.py
@@ -0,0 +1,6 @@
+num1 = float(input("Digite o primeiro numero: "))
+num2 = float(input("Digite o Segundo numero: "))
+
+soma = num1 + num2
+
+print("A soma dos numeros é: ", soma)
